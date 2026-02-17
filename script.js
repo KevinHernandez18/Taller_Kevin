26a9f82b320df3e4698c60fe6c128ddd830a5c8c
KevinHernandez18 <khernandezsierra895@gmail.com>
2026-02-17 11:50:23 -0500

    Se diseña el estilo beta de la página web del portafolios. #2

---
+++ script.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    const skills = document.querySelectorAll(".skill");
+
+    skills.forEach(skill => {
+
+        const percent = skill.getAttribute("data-percent");
+        const progressBar = skill.querySelector(".progreso");
+        const percentText = skill.querySelector(".percent-text");
+
+        if (!progressBar || !percentText) return;
+
+        percentText.textContent = percent + "%";
+        progressBar.style.width = percent + "%";
+
+    });
+
+});
