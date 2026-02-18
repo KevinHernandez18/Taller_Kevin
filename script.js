5815ac3703d04a824447a8d61eedb57c7583a929
KevinHernandez18 <khernandezsierra895@gmail.com>
2026-02-17 22:04:38 -0500

    se ajustan cambios en el código #8 #9

--- script.js
+++ script.js
@@ -23,13 +23,31 @@ columnas.forEach(columna => {
 
     columna.addEventListener("click", () => {
 
-        // quitar activa de todas
         columnas.forEach(col => col.classList.remove("activa"));
 
-        // activar solo la clickeada
         columna.classList.add("activa");
 
     });
 
 });
 
+const imagenes = [
+    "asset/images/mundo.png",
+    "asset/images/LATAM.jpg",
+    "asset/images/tecnologia.jpg",
+    "asset/images/starship.jpeg"
+];
+
+let indice = 0;
+const img = document.getElementById("galeria-img");
+
+setInterval(() => {
+    indice = (indice + 1) % imagenes.length;
+    img.style.opacity = 0;
+
+    setTimeout(() => {
+        img.src = imagenes[indice];
+        img.style.opacity = 1;
+    }, 400);
+
+}, 8000);
